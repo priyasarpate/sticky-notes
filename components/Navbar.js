7bbf79d22cef01b6f05d7724d6dfcf8b8ab3f455
Gunpriya Sarpate <sarpatepriya@gmail.com>
2023-04-17 00:36:51 +0530

    wroking on sticky notes

---
+++ components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import styles from "../styles/Navbar.module.css";
+import styles from '@/styles/Navbar.module.scss'
+
+
+function Navbar() {
+  return (
+    <div>
+      <main className={styles.main}>hello</main>
+    </div>
+  );
+}
+
+export default Navbar;
