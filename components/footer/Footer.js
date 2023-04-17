86602dba61cad59ad8b3f31221ec7c671ec68860
Gunpriya Sarpate <sarpatepriya@gmail.com>
2023-04-17 16:29:02 +0530

    added delete and remove icons

---
+++ components/footer/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div> Made with ♥ by Gunpriya</div>
+  )
+}
+
+export default Footer
--- components/notes/List.js
+++ components/notes/List.js
@@ -1,5 +1,7 @@
 import React from "react";
 import styles from "@/styles/Sticky.module.scss";
+import {FaEraser} from "react-icons/fa";
+import {ImPencil2} from "react-icons/im";
 
 const TodoList = ({ todos, handleDelete, handleEdit }) => {
   return (
@@ -12,8 +14,15 @@ const TodoList = ({ todos, handleDelete, handleEdit }) => {
             <div className={styles.bottom_circle}></div>
           </div>
           <p>{t.todo}</p>
-          {/* <button onClick={() => handleEdit(t.id)}>Edit</button>
-          <button onClick={() => handleDelete(t.id)}>Delete</button> */}
+
+          <div className={styles.icons}>
+          <div onClick={() => handleEdit(t.id)}>
+          <ImPencil2 />
+          </div>
+          <div onClick={() => handleDelete(t.id)}>
+            <FaEraser />
+          </div>
+          </div>
         </div>
       ))}
     </ul>
